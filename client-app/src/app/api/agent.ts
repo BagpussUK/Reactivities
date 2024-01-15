a7ca37b27c4c38a3a54751a6d9f9662893bf91f3
James Slade <James.Slade@ikirupeople.com>
2024-01-15 15:27:07 +0000

    End of Section 20

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -12,7 +12,7 @@ const sleep = (delay: number) => {
     })
 }
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = 'http://localhost:5001/api';
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
@@ -95,7 +95,10 @@ const Profiles = {
     },
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
     deletePhoto: (id: string) => requests.del(`/photos/${id}`),
-    updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile)
+    updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile),
+    updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
+    listFollowings: (username: string, predicate: string) => 
+        requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
 }
 
 const agent = {
